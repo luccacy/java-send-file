53100a3a8fd6e3e02ffcd5543b2a1e2bb0b26e8c
luccacy <luccacy@yahoo.com>
2013-12-10 18:36:20 +0800

    chunk

--- src/Client.java
+++ src/Client.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.channels.FileChannel;
 
 
 public class Client {
@@ -11,7 +12,7 @@ public class Client {
 		
 		try {
 			int i = 0;
-			for(i=0; i<5; i++){
+			for(i=0; i<1; i++){
 				String filename = new String("file" + i + ".img");
 				SendThread th = new SendThread(filename);
 				Thread thread = new Thread(th);
@@ -47,10 +48,18 @@ class SendThread implements Runnable{
 			conn.setReadTimeout(5 * 1000);
 			conn.setDoOutput(true); // 发送POST请求， 必须设置允许输出
 			conn.setUseCaches(false);
+			conn.setChunkedStreamingMode(10 *10 * 1024);
+			
+			FileInputStream fs = new FileInputStream("e:\\ubuntu-12.04-alternate-i386.iso");
+			
+			//FileInputStream fs = new FileInputStream("e:\\test.zip");
+			FileChannel fc = null;
+			
+			fc = fs.getChannel();
 			
 			conn.setRequestProperty("Content-Type", "application/octet-stream");
 			conn.setRequestProperty("Transfer-Encoding", "chunked");
-			conn.setRequestProperty("x-image-meta-size", "11141121");
+			conn.setRequestProperty("x-image-meta-size", ""+fc.size());
 			conn.setRequestProperty("X-Auth-Token",
 					"48bf0956adbd46b6b3867aed56fc2cd4");
 			conn.setRequestProperty("x-image-meta-name", filename);
@@ -58,10 +67,31 @@ class SendThread implements Runnable{
 			
 			DataOutputStream outStream = new DataOutputStream(
 					conn.getOutputStream());
+//			
+//			
+//			
+//			MappedByteBuffer byteBuffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+//			byteBuffer.allocateDirect(10 * 1024 * 1024);
+//			while (byteBuffer.hasRemaining()) {
+//				byte[] tempBuffer = new byte[10 * 1024 * 1024];
+//				if (byteBuffer.remaining() < tempBuffer.length) {
+//					byte[] tempArr = new byte[byteBuffer.remaining()];
+//					byteBuffer.get(tempArr);
+//					outStream.write(tempBuffer);
+//				}
+//				else {
+//					byteBuffer.get(tempBuffer);
+//					outStream.write(tempBuffer);
+//				}
+//				outStream.flush();
+//			}
+
+
 			
 			InputStream inStream;
-			File file = new File("D:\\workspace\\python\\java-upload-client", "cirros.img");
+			File file = new File("e:\\test.zip");
 			inStream = new FileInputStream(file);
+
 			System.out.println("begin");
 			byte[] buffer =new byte[10 *1024];
 			int rlen = 0;
@@ -71,6 +101,7 @@ class SendThread implements Runnable{
 			while(nlen > 0){
 				if(nlen >= 10*1024){			
 					rlen = inStream.read(buffer, 0, 10*1024);
+					
 					outStream.write(buffer);
 					nlen = nlen - rlen;
 				}else{
